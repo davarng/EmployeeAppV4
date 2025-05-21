053e47bab41bd32d1b9b7d3b535b929674882da0
davarng <113583854+davarng@users.noreply.github.com>
2025-05-21 11:57:57 +0200

    commentw

--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -13,6 +13,10 @@ namespace EmployeesApp.Web
             //builder.Services.AddScoped<EmployeeService>();
             builder.Services.AddSingleton<EmployeeService>();//Employee is Added to the list because it is create one employee service.
 
+            // 1E. It is easier to change which service is used so you can use different ones for testing while only changing one line of code.
+            //     Lifespan of the service can easily be controlled by the builder.Services by changing it to AddTransient, AddScoped or AddSingleton.
+            //     Easier to maintain and test the code.
+
             var app = builder.Build();
             app.UseStaticFiles();
             app.MapControllers();
